ebc5b4c6da1201563e893ffb68d957b10654ade6
AABur <aabur@mail.ru>
2020-08-18 21:38:24 +0200

    welcome_user
    added welcome_user

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+from brain_games.cli import welcome_user
+
+
 def main():
     return('Welcome to the Brain Games!')
 
 
-print('test - bg')
+print(main())
+
+welcome_user()
 
 if __name__ == '__main__':
     main()
