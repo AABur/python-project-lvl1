6ff1d138a824f0c8750119ffd8eb2d364befebad
AABur <aabur@mail.ru>
2020-08-25 02:34:02 +0200

    autoformating

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,12 +2,12 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
+    name = prompt.string("May I have your name? ")
+    print("Hello, {}!".format(name))
     print()
-    return(name)
+    return name
 
 
 def answer():
-    ans = prompt.string('Your answer: ')
-    return(ans)
+    ans = prompt.string("Your answer: ")
+    return ans
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -7,24 +7,34 @@ from brain_games.even import even
 
 
 def main():
-    print('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n')
+    print(
+        'Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n'
+    )
     name = welcome_user()
     i = 1
     while i < 4:
         num = random.randint(1, 100)
-        print('Question: {}'.format(num))
+        print("Question: {}".format(num))
         ans = answer()
-        if (even(num) and ans == 'yes') or (not even(num) and ans == 'no'):
-            print('Correct!')
-        elif ans == 'yes':
-            return(print('"yes" is wrong answer ;(. Correct answer was "no".\nLet\'s try again, {}!'.format(name)))
-        elif ans == 'no':
-            return(print('"no" is wrong answer ;(. Correct answer was "yes".\nLet\'s try again, {}!'.format(name)))
+        if (even(num) and ans == "yes") or (not even(num) and ans == "no"):
+            print("Correct!")
+        elif ans == "yes":
+            return print(
+                '"yes" is wrong answer ;(. Correct answer was "no".\nLet\'s try again, {}!'.format(
+                    name
+                )
+            )
+        elif ans == "no":
+            return print(
+                '"no" is wrong answer ;(. Correct answer was "yes".\nLet\'s try again, {}!'.format(
+                    name
+                )
+            )
         else:
-            return(print('Wrong answer ;(. \nLet\'s try again,{}!'.format(name)))
+            return print("Wrong answer ;(. \nLet's try again,{}!".format(name))
         i += 1
-    print('Congratulations, {}!'.format(name))
+    print("Congratulations, {}!".format(name))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
