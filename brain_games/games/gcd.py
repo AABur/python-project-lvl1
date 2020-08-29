3000559a4378466ef881bc98255970c0b2a086e5
AABur <aabur@mail.ru>
2020-08-30 02:05:50 +0300

    refactor(gcd): minor refactoring

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,18 +1,25 @@
 """GCD game logic."""
 
-
 from math import gcd
 from random import randint
 
+GAME_DESCRIPTION = "Find the greatest common divisor of given numbers."
+MIN_NUM = 1
+MAX_NUM = 20
+
 
 def game_gcd():
-    """GCD game logic.
+    """GCD game Q&A generation.
+
+    generate two random Numbers
+    and calculate greatest common divider for them
 
     Returns:
-        str: task, correct answer
+        task{str} : Numbers;
+        answer{str} : greatest common divider
     """
-    num1 = randint(1, 100)
-    num2 = randint(1, 100)
+    num1 = randint(MIN_NUM, MAX_NUM)
+    num2 = randint(MIN_NUM, MAX_NUM)
     correct_answer = gcd(num1, num2)
     task = "{} {}".format(num1, num2)
     return (str(task), str(correct_answer))
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -2,12 +2,12 @@
 """GCD game script."""
 
 from brain_games.engine import run_game
-from brain_games.games.gcd import game_gcd
+from brain_games.games.gcd import GAME_DESCRIPTION, game_gcd
 
 
 def main():
     """GCD game script."""
-    run_game(game_gcd, "Find the greatest common divisor of given numbers.")
+    run_game(game_gcd, GAME_DESCRIPTION)
 
 
 if __name__ == "__main__":
