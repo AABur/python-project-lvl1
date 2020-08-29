11b040bcab1fc7b209c6835645650c0d0a4bb720
AABur <aabur@mail.ru>
2020-08-30 01:41:33 +0300

    (even): minor refactoring

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,14 +1,21 @@
 """Even game logic."""
 from random import randint
 
+GAME_DESCRIPTION = "Answer 'yes' if number even otherwise answer 'no'."
+MIN_NUM = 1
+MAX_NUM = 20
+
 
 def game_even():
-    """Even game logic.
+    """Even game Q&A generation.
+
+    generate random Number and check if a Number is Even
 
     Returns:
-        str: task, correct answer
+        task{str} : Number;
+        answer{str} : Even status
     """
-    num = randint(1, 100)
+    num = randint(MIN_NUM, MAX_NUM)
     answer = ("yes", "no")
     correct_answer = answer[num % 2]
     task = "{}".format(num)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,15 +2,12 @@
 """Even game script."""
 
 from brain_games.engine import run_game
-from brain_games.games.even import game_even
+from brain_games.games.even import GAME_DESCRIPTION, game_even
 
 
 def main():
     """Even game script."""
-    run_game(
-        game_even,
-        'Answer \033[31m"yes"\033[m if number even otherwise answer \033[31m"no"\033[m',
-    )
+    run_game(game_even, GAME_DESCRIPTION)
 
 
 if __name__ == "__main__":
