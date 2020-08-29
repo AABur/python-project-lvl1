ff0c590acacbb47023a97297c50c50e681509e02
AABur <aabur@mail.ru>
2020-08-30 02:52:10 +0300

    (engine): minor changes

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,16 +8,20 @@ from brain_games.cli import (
     wrong_answer,
 )
 
+STEPS = 3  # number of game steps
+
 
 def run_game(game_name, game_description):
-    """Runing the game.
+    """Brain Games Engine.
+
+    Executing game process
 
     Args:
-        game_name : game module from brain_games/games
-        game_description : game description
+        game_name (function): game logic module
+        game_description (str): game rules
     """
     user_name = welcome_dialog(game_description)
-    for _ in range(3):
+    for _ in range(STEPS):
         (game_task, game_answer) = game_name()
         user_answer = qa_dialog(game_task)
         if user_answer != game_answer:
