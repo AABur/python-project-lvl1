466ca18337b6032b6b8e97752f4dcfaaca219ed2
AABur <aabur@mail.ru>
2020-08-30 03:15:47 +0300

    style(cli): minor changes

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -12,8 +12,7 @@ def welcome_dialog(task_msg):
     Returns:
         (str): User Name
     """
-    print("Welcome to the Brain Games!")
-    print("{}\n".format(task_msg))
+    print("Welcome to the Brain Games!\n{}\n".format(task_msg))
     name = prompt.string("May I have your name? ")
     print("Hello, {}!\n".format(name))
     return name
@@ -40,27 +39,24 @@ def congratulations(user_name):
     Args:
         user_name (str): User name
     """
-    print("\033[31mCongratulations, \033[1m{}\033[0;31m!\033[m".format(user_name))
+    print("Congratulations, {}".format(user_name))
 
 
 def wrong_answer(user_answer, correct_answer, user_name):
     """Wrong answer info fo user.
 
-    Inform user about correct answer
+    Inform user about wrong answer
     Interupt the game
 
     Args:
         user_answer (str): User Answer
         correct_answer (str): Correct Answer
-        user_name (str): User Answer
+        user_name (str): User Name
     """
-    print(
-        '\033[31m"{}"\033[m is wrong answer ;(. Correct answer was \033[31m"{}"\033[m.'
-        .format(user_answer, correct_answer)
-    )
-    print("Let's try again, \033[1m{}\033[m!".format(user_name))
+    print("'{}' is wrong answer ;(. Correct answer was '{}'.".format(user_answer, correct_answer))
+    print("Let's try again, {}".format(user_name))
 
 
 def correct():
-    """Just Correct"""
+    """Just Correct."""
     print("Correct!")
