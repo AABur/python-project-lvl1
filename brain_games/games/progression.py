f6f0f603c02fefdd24e9161161967dc5535a3988
AABur <aabur@mail.ru>
2020-09-04 22:07:31 +0300

    refactor: changed imported functions call
    Imported functions call refactored to {module.function}

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,8 +1,9 @@
 """Progression game engine."""
 
-from random import randint
+import random
 
 GAME_DESCRIPTION = "What number is missing in the progression?"
+
 START_MIN = 1
 START_MAX = 20
 STEP_MIN = 1
@@ -20,11 +21,11 @@ def run_progression():
         task{str} : arithmetic progression;
         answer{str} : hidden number
     """
-    start = randint(START_MIN, START_MAX)
-    step = randint(STEP_MIN, STEP_MAX)
+    start = random.randint(START_MIN, START_MAX)
+    step = random.randint(STEP_MIN, STEP_MAX)
     a_progression = [(start + (ind * step))
                      for ind in range(PROGERSSION_LENGTH)]
-    hidden = randint(1, PROGERSSION_LENGTH)
+    hidden = random.randint(1, PROGERSSION_LENGTH)
     answer = str(a_progression[hidden - 1])
     a_progression[hidden - 1] = ".."
     task = " ".join(str(ind) for ind in a_progression)
