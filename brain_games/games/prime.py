234616d846e026239ddfb69d42d44ddc8961b6b8
AABur <aabur@mail.ru>
2020-09-08 17:01:36 +0300

    refactor: implemented  prime number check function
    implemented  prime number check functionr instead of using external

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,8 +2,6 @@
 
 import random
 
-import pyprimer
-
 GAME_DESCRIPTION = "Answer 'yes' if number prime otherwise answer 'no'."
 
 MIN_NUM = 1
@@ -20,6 +18,25 @@ def get_task():
         answer{str} : Prime status
     """
     num = random.randint(MIN_NUM, MAX_NUM)
-    answer = "yes" if pyprimer.is_prime(num) else "no"
+    answer = "yes" if is_prime(num) else "no"
     task = "{}".format(num)
     return (task, answer)
+
+
+def is_prime(num):
+    """Prime number check.
+
+    Args:
+        num (int): number to check
+
+    Returns:
+        bool: True or False
+    """
+    if num < 2:
+        return False
+    index = 2
+    while index <= num // 2:
+        if num % index == 0:
+            return False
+        index += 1
+    return True
