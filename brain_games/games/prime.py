3d7e70438d43c2f1a55d7de90c6aef3f4ef617cc
AABur <aabur@mail.ru>
2020-09-30 20:33:25 +0300

    Revert "Revert "refactor: redundant functions removed, code simplified""
    This reverts commit de0e0e380248eb605ec075fd2d107d60c48b6fca.

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,25 +2,25 @@
 
 import random
 
-GAME_DESCRIPTION = "Answer 'yes' if number prime otherwise answer 'no'."
+GAME_DESCRIPTION = 'Answer "yes" if number prime otherwise answer "no".'
 
 MIN_NUM = 1
-MAX_NUM = 20
+MAX_NUM = 100
 
 
-def get_task():
+def get_challenge():
     """Prime game Q&A generation.
 
     generate random Number and check if a Number is Prime
 
     Returns:
-        task{str} : Number;
+        question{str} : Number;
         answer{str} : Prime status
     """
     num = random.randint(MIN_NUM, MAX_NUM)
-    answer = "yes" if is_prime(num) else "no"
-    task = "{}".format(num)
-    return (task, answer)
+    answer = 'yes' if is_prime(num) else 'no'
+    question = '{}'.format(num)
+    return (question, answer)
 
 
 def is_prime(num):
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -10,5 +10,5 @@ def main():
     run_game(prime)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
